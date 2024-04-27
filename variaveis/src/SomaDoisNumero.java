3f5513ff022a5cf095d135590cab3c5944876369
vitinh0z <victor10.vg23@gmail.com>
2024-04-27 19:52:50 -0300

    soma de dois números

---
+++ variaveis/src/SomaDoisNumero.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+    public class SomaDoisNumero {
+
+         public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner (System.in);
+
+        // Solicita que digite o primeiro número
+        System.out.println("DIgite o Primeiro Número: ");
+
+        int numero1 = scanner.nextInt();
+
+        // Solicita que digite o segundo número
+        System.out.println("Digite o Segundo Número: ");
+
+        int numero2 = scanner.nextInt();
+        int soma = numero1 + numero2;
+
+    System.out.println("A soma entre de " + numero1 + " e "  + numero2 + " é " + soma );
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
