4e3d2669c8c8a4a16fb9e912b4b2b2a7321d589c
vitinh0z <victor10.vg23@gmail.com>
2024-05-21 13:14:52 -0300

    simulando conta de banco com valores já determinados

---
+++ variaveis/ContaBanco/src/ContaBanco.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+    public class ContaBanco {
+
+    public static void main(String[] args) {
+        
+
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.println("Por favor, digite o seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite sua Agencia: ");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Digite Número: ");
+        int numero = scanner.nextInt();
+
+    if (nome.equals("Mario Andrade") && agencia.equals ("0678") && numero == 1021) {
+        System.out.println("Olá Mario Andrade, seu salfdo em conta é 237.48");
+    }
+    else {
+        System.out.println("Usuario não encontrado. Tente novamente mais tarde");
+    }
+
+        }
+
+
+
+
+
+
+    }
+
